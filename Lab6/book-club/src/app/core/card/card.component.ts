d1eeec601a57ccd1ffbc65bb3d8a94deb03dd03e
Ana Calugareanu <144672917+AnaCalugareanu@users.noreply.github.com>
2025-05-19 17:01:53 +0300

    feat: implement book loading, wishlist, and review functionalities in home and card components (#15)

--- Lab6/book-club/src/app/core/card/card.component.ts
+++ Lab6/book-club/src/app/core/card/card.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { BookService } from '../../shared/bookService';
 
 @Component({
   selector: 'app-card',
@@ -13,4 +14,18 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 })
 export class CardComponent {
   @Input() book: any;
+
+  constructor(private bookService: BookService) {}
+
+  handleReview() {
+    const review = {
+      bookTitle: this.book.title,
+      bookAuthor: this.book.authorName,
+      reviewText: 'This is a sample review text.',
+    };
+    this.bookService.addReview(review);
+  }
+  handleAddToWishlist() {
+    this.bookService.addToWishlist(this.book);
+  }
 }
--- Lab6/book-club/src/app/shared/bookService.ts
+++ Lab6/book-club/src/app/shared/bookService.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
   private apiUrl = 'https://openlibrary.org/search.json';
 
+  private wishlistSubject = new BehaviorSubject<any[]>([]);
+  wishlist$ = this.wishlistSubject.asObservable();
+
+  private reviewsSubject = new BehaviorSubject<any[]>([]);
+  reviews$ = this.reviewsSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
-  // Fetch books from OpenLibrary with optional search query
   getBooks(query: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}?q=${encodeURIComponent(query)}`);
+  }
+
+  addToWishlist(book: any): void {
+    const currentWishlist = this.wishlistSubject.value;
+    if (
+      !currentWishlist.find(
+        (b) => b.title === book.title && b.authorName === book.authorName
+      )
+    ) {
+      this.wishlistSubject.next([...currentWishlist, book]);
+    }
+  }
+
+  // Add a review written by the user
+  addReview(review: any): void {
+    const currentReviews = this.reviewsSubject.value;
+    this.reviewsSubject.next([...currentReviews, review]);
   }
 }
