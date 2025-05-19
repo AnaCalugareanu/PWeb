83e7b043b35dabd0c8ef90329600adfdfedf21b2
Ana Calugareanu <144672917+AnaCalugareanu@users.noreply.github.com>
2025-05-19 14:35:05 +0300

    feat: integrate ng-zorro-antd components and setup routing (#13)
    - Added HeaderComponent with navigation links to Home and Favorites.
    - Implemented HomeComponent and FavoritesComponent with basic templates.
    - Configured app routing to redirect to Home and load components lazily.
    - Updated app configuration to include necessary providers for ng-zorro-antd and forms.
    - Imported Tailwind CSS and ng-zorro-antd styles in global styles.
    - Created PostCSS configuration for Tailwind CSS.
    - Added custom theming for ng-zorro-antd.

---
+++ Lab6/book-club/src/app/core/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NzMenuModule, NzIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
